63de2332a078116696b3c7147308463979177f03
Niladri <niladrisengit12@gmail.com>
2024-08-25 01:09:51 +0530

    Prepared statement

--- src/Demo.java
+++ src/Demo.java
@@ -6,20 +6,41 @@ public class Demo {
         String url = "jdbc:postgresql://localhost:5433/students";
         String user = "postgres";
         String password = "user";
-        String query = "SELECT * FROM student";
+        String query = "SELECT * FROM student ";
         String inserQuery = "INSERT INTO student VALUES(4, 'Rahul')";
+        String preparedInsertQuery = "INSERT INTO student VALUES(?, ?)";
         String updateQuery = "UPDATE student SET sname = 'Raj' WHERE sid = 4";
+        String preparedUpdateQuery = "UPDATE student SET sname = ? WHERE sid = ?";
         String deleteQuery = "DELETE FROM student WHERE sid = 4";
         Connection conn = null;
 
         conn = DriverManager.getConnection(url, user, password);
         System.out.println("Connection established");
 
-        // Do something with the connection
+        // Manually inserting data
         Statement stmt = conn.createStatement();
-//        stmt.executeUpdate(inserQuery);
+        //stmt.executeUpdate(inserQuery);
+
+        // Using prepared statement to insert Dynamic Data
+        PreparedStatement pstmt = conn.prepareStatement(preparedInsertQuery);
+        int id = 125;
+        String name = "Paramita";
+        pstmt.setInt(1, id);
+        pstmt.setString(2, name);
+        pstmt.executeUpdate();
+
+        //Manually updating and deleting data
         stmt.executeUpdate(updateQuery);
         stmt.executeUpdate(deleteQuery);
+
+        //Dynamic update and delete
+        PreparedStatement updatePreparedQuery = conn.prepareStatement(preparedUpdateQuery);
+        int updateId = 3;
+        String updateName = "Anonymous";
+        updatePreparedQuery.setString(1, updateName);
+        updatePreparedQuery.setInt(2, updateId);
+        updatePreparedQuery.executeUpdate();
+
         ResultSet rs = stmt.executeQuery(query);
 //        System.out.println(rs.next());
 //        System.out.println("Name is "+rs.getString("sname"));
